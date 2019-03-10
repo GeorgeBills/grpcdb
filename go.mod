40304c0dbbd94e635fecf49a25b6e9453178d9c1
George Bills <george.s.bills@gmail.com>
2019-03-10 21:07:05 +1100

    Database connection working.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/GeorgeBills/grpcdb
 
-require github.com/golang/protobuf v1.3.0
+require (
+	github.com/golang/protobuf v1.3.0
+	github.com/lib/pq v1.0.0
+	golang.org/x/net v0.0.0-20180906233101-161cd47e91fd
+	google.golang.org/grpc v1.19.0
+)
--- server/server.go
+++ server/server.go
@@ -2,29 +2,53 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"github.com/GeorgeBills/grpcdb"
 	"github.com/GeorgeBills/grpcdb/api"
+	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 )
 
 const (
-	listen = ":1234"
+	listen         = ":1234"
+	dataSourceName = "host=127.0.0.1 port=5432 user=postgres password=chbqkWQQkgEJh2 dbname=postgres sslmode=disable"
 )
 
 func main() {
+	// listen on socket
 	lis, err := net.Listen("tcp", listen)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// get database connection
+	db, err := sql.Open("postgres", dataSourceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// check that the database is connected
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Database connected")
+
+	// start server
 	server := grpc.NewServer()
-	handler := &handler{}
+	handler := &handler{
+		db,
+	}
 	grpcdbpb.RegisterGRPCDBServer(server, handler)
 	server.Serve(lis)
 }
 
-type handler struct{}
+type handler struct {
+	db *sql.DB
+}
 
 func (h *handler) Query(ctx context.Context, statement *grpcdbpb.Statement) (*grpcdbpb.Result, error) {
 	log.Printf("Received statement: %+v", statement)
@@ -34,5 +58,10 @@ func (h *handler) Query(ctx context.Context, statement *grpcdbpb.Statement) (*gr
 		return nil, err
 	}
 	log.Printf("Running statement: %s", sql)
+	res, err := h.db.Exec(sql)
+	if err != nil {
+		log.Printf("Error running statement: %v", err)
+	}
+	log.Print(res)
 	return &grpcdbpb.Result{}, nil
 }
